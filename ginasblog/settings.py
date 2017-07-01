0bd7e8711564f01f34cc617b359f27d320d93c3a
gina iliff <Giliff@me.com>
2017-06-30 19:36:04 -0700

    added base.html

--- ginasblog/settings.py
+++ ginasblog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ginasblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
